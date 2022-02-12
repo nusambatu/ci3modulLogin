b047137d20143cac920a9724d3a86a885faaf61d
unknown <nusambatu@gmail.com>
2022-02-12 20:51:29 +0700

    hmvc

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -6,7 +6,7 @@
                 <div class="sidebar-brand-icon rotate-n-15">
                     <i class="fas fa-code"></i>
                 </div>
-                <div class="sidebar-brand-text mx-3">NUSAMBATU nusambatu</div>
+                <div class="sidebar-brand-text mx-3">NUSAMBATU</div>
             </a>
 
             <!-- Divider -->
